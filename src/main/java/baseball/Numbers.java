aefbf076bafe297ca444a5b145f09aad872c9c81
isemang <a55306039@gmail.com>
2021-11-10 12:37:11 +0900

    refactor : 댓글 참고 후 Numbers 객체 refactor - LENGTH 변수 추가 - size() 함수 삭제 - 구체적인 구현체인 HashSet이 아닌 Set에 할당하도록 변경, 유연성 증가

--- src/main/java/baseball/Numbers.java
+++ src/main/java/baseball/Numbers.java
@@ -2,46 +2,41 @@ package baseball;
 
 import nextstep.utils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * 게임 시작 시 random한 숫자를 만들고, 저장하여 가지고 있는 객체
  * 객체 선언 시 자리수(length)를 받는다
  */
 public class Numbers {
-    private ArrayList<Integer> numList;
+    public static final int LENGTH = 3;
+    private List<Integer> numList;
 
-    Numbers(int length) {
+    Numbers() {
         numList = new ArrayList<>();
-        makeNumber(length);
-    }
-
-    public int size() {
-        return numList.size();
+        makeNumber();
     }
 
     public int[] getNumbers() {
-        int[] array = new int[size()];
+        int[] array = new int[LENGTH];
 
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < LENGTH; i++) {
             array[i] = numList.get(i);
         }
 
         return array;
     }
 
-    private void makeNumber(int length) {
-        HashSet<Integer> set = new HashSet<>();
+    private void makeNumber() {
+        Set<Integer> set = new HashSet<>();
 
-        while(set.size() < length) {
+        while(set.size() < LENGTH) {
             int randInt = Randoms.pickNumberInRange(1,9);
             set.add(randInt);
         }
 
         //항상 작은숫자가 앞자리에 나오는 걸 방지
-        ArrayList<Integer> list = new ArrayList<>(set);
+        List<Integer> list = new ArrayList<>(set);
         Collections.shuffle(list);
         numList = list;
     }
