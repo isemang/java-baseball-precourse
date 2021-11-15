85e18839e2f0532697ef5e868224ccf606276339
isemang <a55306039@gmail.com>
2021-11-15 15:43:40 +0900

    fix : 축약어 변경, countUtil 이름 변경 - 축약어 쓰지 않도록 변경(cnt -> count, idx -> index)

--- src/main/java/nextstep/utils/Judge.java
+++ src/main/java/nextstep/utils/Judge.java
@@ -4,15 +4,15 @@ import baseball.Result;
 
 import static baseball.Numbers.LENGTH;
 
-public class CountUtil {
-    private static int strikeCnt = 0;
-    private static int ballCnt = 0;
+public class Judge {
+    private static int strikeCount = 0;
+    private static int ballCount = 0;
 
-    private CountUtil() {
+    private Judge() {
     }
 
     public static Result compare(String number, String attackNum) {
-        initializeCnt();
+        initializeCount();
 
         for (int i = 0; i < LENGTH; i++) {
             String str = Character.toString(number.charAt(i));
@@ -20,27 +20,27 @@ public class CountUtil {
             strikeOrBall(attackNum, str, i);
         }
 
-        return new Result(strikeCnt, ballCnt);
+        return new Result(strikeCount, ballCount);
     }
 
-    private static void initializeCnt() {
-        strikeCnt = 0;
-        ballCnt = 0;
+    private static void initializeCount() {
+        strikeCount = 0;
+        ballCount = 0;
     }
 
     private static void strikeOrBall(String attackNum, String str, int i) {
         if (attackNum.contains(str)) {
-            int idx = attackNum.indexOf(str);
+            int index = attackNum.indexOf(str);
 
-            updateCnt(i, idx);
+            updateCnt(i, index);
         }
     }
 
-    private static void updateCnt(int i, int idx) {
-        if(i == idx) {
-            strikeCnt++;
+    private static void updateCnt(int i, int index) {
+        if(i == index) {
+            strikeCount++;
             return;
         }
-        ballCnt++;
+        ballCount++;
     }
 }
